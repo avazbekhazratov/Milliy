30e48ac5206060b415ca18f60e19ee3b5c3c1b13
Avazbek Hazratov <127407718+avazbekhazratov@users.noreply.github.com>
2023-10-23 17:02:57 +0500

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,22 +1,22 @@
 from .users import User
 from app.models.core import (
-    Category,
-    SubCategory,
-    Main,
-    Partners,
-    Partners_logo,
-    Advert,
-    News,
-    News_add,
-    Xaridordan_savol,
-    Company,
-    Contact,
-    Brend,
-    Product_info,
-    Xarakteristika,
-    Opesaniya,
-    FeedBack,
-    Savol_Javoblar,
-    Product,
-    Product_images
+    Category, #
+    SubCategory, #
+    Main,#
+    Partners,#
+    Partners_logo, # xusan
+    Advert, # xusan
+    News, # xusan
+    News_add, # Donyor
+    Xaridordan_savol,  # Donyor
+    Company, # Avaz
+    Contact, # Avaz
+    Brend,  # Avaz
+    Product_info,  # Donyor
+    Xarakteristika, # Akbar
+    Opesaniya,  # Akbar
+    FeedBack,  # Akbar
+    Savol_Javoblar, #
+    Product, #
+    Product_images  # Akbar
 )
